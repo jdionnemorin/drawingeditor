e0217faa1b8e1875256f1c449e79cf6ba07b8179
Jonathan Dionne-Morin <jodionne.morin@videotron.ca>
2017-05-19 14:37:29 -0400

    added colorpicker, slider for the pen size

--- public/drawings.js
+++ public/drawings.js
@@ -1,14 +1,24 @@
-var canvas;
-var socket;
-var context;
+var canvas, socket, context;
+
 var stroke = {
 	color: '#000000',
-	width: 2,
+	width: 2
 };
+// 1 = line 2 = rect
+var type = 'rect';
+
 
 var reset = function() {
 	socket.emit('reset');
 	context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+var changeColor = function() {
+	stroke.color = document.getElementById('color').value;
+};
+
+var changeWidth = function() {
+	stroke.width = document.getElementById('width').value;
 };
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -43,11 +53,25 @@ document.addEventListener("DOMContentLoaded", function() {
 	socket.on('draw_line', function (data) {
 		var line = data.line;
 		context.strokeStyle = data.stroke.color;
+		context.lineCap = "round";
 		context.beginPath();
 		context.lineWidth = data.stroke.width;
 		context.moveTo(line[0].x * width, line[0].y * height);
 		context.lineTo(line[1].x * width, line[1].y * height);
 		context.stroke();
+		context.closePath();
+	});
+
+	socket.on('draw_rect', function (data) {
+		console.log('ok');
+		var line = data.line;
+		context.strokeStyle = data.stroke.color;
+		context.lineWidth = data.stroke.width;
+		var x = Math.min(line[0].x * width,  line[1].x * width),
+			y = Math.min(line[0].y * height,  line[1].y * height),
+			w = Math.abs(line[0].x * width - line[1].x * width),
+			h = Math.abs(line[0].y * height - line[1].y * height);
+		context.strokeRect(x, y, w, h);
 	});
 
 	socket.on('reset', function () {
@@ -56,7 +80,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	function mainLoop() {
 		if (mouse.click && mouse.move && mouse.pos_prev) {
-			socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ], stroke: stroke });
+			switch(type) {
+				case 'pen' :
+					socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ], stroke: stroke });
+					break;
+				case 'rect' :
+					socket.emit('draw_rect', { line: [ mouse.pos, mouse.pos_prev ], stroke: stroke });
+					break;
+				default:
+					socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ], stroke: stroke });
+			}
+
 			mouse.move = false;
 		}
 		mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
--- server.js
+++ server.js
@@ -10,21 +10,36 @@ app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
 
 var line_history = [];
-var strokeHistory = [];
+var lineStrokeHistory = [];
+
+var rect_history = [];
+var rectStrokeHistory = [];
 
 io.on('connection', function (socket) {
 	for (var i in line_history) {
-		socket.emit('draw_line', { line: line_history[i], stroke: strokeHistory[i] } );
+		socket.emit('draw_line', { line: line_history[i], stroke: lineStrokeHistory[i] } );
+	}
+	for (var i in rect_history) {
+		socket.emit('draw_line', { line: rect_history[i], stroke: rectStrokeHistory[i] } );
 	}
 	socket.on('draw_line', function (data) {
 		line_history.push(data.line);
-		strokeHistory.push(data.stroke);
+		lineStrokeHistory.push(data.stroke);
+		io.emit('draw_line', { line: data.line, stroke: data.stroke});
+	});
+
+	socket.on('draw_rect', function (data) {
+		rect_history.push(data.line);
+		rectStrokeHistory.push(data.stroke);
 		io.emit('draw_line', { line: data.line, stroke: data.stroke});
 	});
 
 	socket.on('reset', function() {
 		line_history = [];
-		strokeHistory = [];
+		lineStrokeHistory = [];
+		rect_history = [];
+		rectStrokeHistory = [];
+
 		io.emit('reset');
 	});
 });
