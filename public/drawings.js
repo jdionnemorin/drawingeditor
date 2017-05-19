ba4f996542ae3839aa111629fa433d7be972f8f5
Jonathan Dionne-Morin <jodionne.morin@videotron.ca>
2017-05-19 12:37:23 -0400

    add navbar with drawing feature

--- public/drawings.js
+++ public/drawings.js
@@ -1,3 +1,16 @@
+var canvas;
+var socket;
+var context;
+var stroke = {
+	color: '#000000',
+	width: 2,
+};
+
+var reset = function() {
+	socket.emit('reset');
+	context.clearRect(0, 0, canvas.width, canvas.height);
+};
+
 document.addEventListener("DOMContentLoaded", function() {
 	var mouse = {
 		click: false,
@@ -5,12 +18,11 @@ document.addEventListener("DOMContentLoaded", function() {
 		pos: {x:0, y:0},
 		pos_prev: false
 	};
-
-	var canvas  = document.getElementById('drawing');
-	var context = canvas.getContext('2d');
+	canvas = document.getElementById('drawing');
+	socket  = io.connect();
+	context = canvas.getContext('2d');
 	var width   = window.innerWidth;
 	var height  = window.innerHeight;
-	var socket  = io.connect();
 
 	canvas.width = width;
 	canvas.height = height;
@@ -30,16 +42,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	socket.on('draw_line', function (data) {
 		var line = data.line;
+		context.strokeStyle = data.stroke.color;
 		context.beginPath();
-		context.lineWidth = 2;
+		context.lineWidth = data.stroke.width;
 		context.moveTo(line[0].x * width, line[0].y * height);
 		context.lineTo(line[1].x * width, line[1].y * height);
 		context.stroke();
 	});
 
+	socket.on('reset', function () {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	});
+
 	function mainLoop() {
 		if (mouse.click && mouse.move && mouse.pos_prev) {
-			socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+			socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ], stroke: stroke });
 			mouse.move = false;
 		}
 		mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
--- server.js
+++ server.js
@@ -10,13 +10,21 @@ app.use(express.static(__dirname + '/public'));
 console.log("Server running on 127.0.0.1:8080");
 
 var line_history = [];
+var strokeHistory = [];
 
 io.on('connection', function (socket) {
 	for (var i in line_history) {
-		socket.emit('draw_line', { line: line_history[i] } );
+		socket.emit('draw_line', { line: line_history[i], stroke: strokeHistory[i] } );
 	}
 	socket.on('draw_line', function (data) {
 		line_history.push(data.line);
-		io.emit('draw_line', { line: data.line });
+		strokeHistory.push(data.stroke);
+		io.emit('draw_line', { line: data.line, stroke: data.stroke});
+	});
+
+	socket.on('reset', function() {
+		line_history = [];
+		strokeHistory = [];
+		io.emit('reset');
 	});
 });
